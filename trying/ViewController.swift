9c0a4f00e5989738fb55dabd39fbe182103876ca
Mahmoud Arafat <mahmoud3030arafat@gmail.com>
2021-05-05 20:49:31 -0700

    Initial Commit

---
+++ trying/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  trying
+//
+//  Created by Mahmoud on 5/5/21.
+//  Copyright © 2021 mahmoud. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

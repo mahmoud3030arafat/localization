d550d754f9e7d968fea721b71cbd5f7e3cf3ebd2
Mahmoud Arafat <mahmoud3030arafat@gmail.com>
2021-05-07 18:10:08 -0700

    intial commit

--- trying/ViewController.swift
+++ trying/ViewController.swift
@@ -9,12 +9,50 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var email: UITextField!
+    
+    @IBOutlet weak var password: UITextField!
+    
+    
 
+    var dataTask : URLSessionTask?
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        email.text?.localized()
+        password.text?.localized()
+         
+        
+        let button : UIButton = {
+            let btn = UIButton(frame: CGRect(x: 0, y: 0, width: 150, height: 44))
+            btn.setTitle("EN", for: .normal)
+            btn.contentHorizontalAlignment = .left
+            btn.setTitleColor(.blue, for: .normal)
+            btn.addTarget(self, action: #selector(pressed), for: .touchUpInside)
+            return btn
+            
+        }()
+        
+        
+        self.navigationItem.leftBarButtonItem = UIBarButtonItem(customView: button)
+        
+        
+        
+        
+
+
     }
+    @objc func pressed(){
+        
+       
+    }
+}
 
-
+extension String{
+func localized()->String{
+    
+    return NSLocalizedString(self, tableName: "Localizable", bundle: .main, value: self, comment: self)
+}
+    
 }
 
